45465ebe6a63e12c064cf95a5abf68be27d1a9fe
markusmikely <markusmikely@gmail.com>
2023-08-03 21:23:42 +0100

    Added basic init functionality

--- machine.py
+++ machine.py
@@ -1,8 +1,26 @@
+from reel import *
+from settings import *
 import pygame
 
+
 class Machine:
-    def __init__(self) -> None:
-        pass
+    def __init__(self):
+        self.display_surface = pygame.display.get_surface()
+        self.reel_index = 0
+        self.reel_list = {}
+
+        self.spawn_reels()
+
+    def spawn_reels(self):
+        if not self.reel_list:
+            x_topleft, y_topleft = 0, -SYMBOL_SIZE  + 0
+        while self.reel_index < 5:
+            if self.reel_index > 0:
+                x_topleft, y_topleft = x_topleft + (SYMBOL_SIZE + X_OFFSET), y_topleft
+            self.reel_list[self.reel_index] = Reel((x_topleft, y_topleft)) # Need to create Reel class
+            self.reel_index += 1
 
     def update(self, delta_time):
-        pass
+        for reel in self.reel_list:
+            self.reel_list[reel].symbol_list.draw(self.display_surface)
+            self.reel_list[reel].symbol_list.update()
--- settings.py
+++ settings.py
@@ -1,10 +1,21 @@
-from unittest.mock import DEFAULT
-
-
-DEFAULT_FILE_IMAGE_SIZE = (300, 300)
+DEFAULT_FILE_IMAGE_SIZE = (250, 250)
 FPS = 120
 HEIGHT = 1000
-WIDTH = 1600
+WIDTH = 1400
+SYMBOL_SIZE = 250
+START_X, START_Y = 0, -SYMBOL_SIZE
+X_OFFSET, Y_OFFSET = 0, 0
 
 # Images
-BG_IMAGE_PATH = "graphics/neon/bg.jpg"
+BG_IMAGE_PATH = "graphics/neon/0-bg.jpg"
+GAME_INDICIES = [1, 2, 3] # 0 and 4 are outside of play area
+SYM_PATH = "graphics/0/symbols"
+
+# 5 Symbols
+symbols = {
+    'blackswan': f"{SYM_PATH}/blackswan.png",
+    'whiteswan': f"{SYM_PATH}/whiteswan.png",
+    'duck': f"{SYM_PATH}/duck.png",
+    'fish': f"{SYM_PATH}/fish.png",
+    'frog': f"{SYM_PATH}/frog.png"
+}
